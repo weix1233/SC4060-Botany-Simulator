b19d52d98d1545d27ea3a6426a031383a1c63161
Wei Chuan <weix1233@gmail.com>
2025-04-01 09:34:16 +0800

    UI for plant state
    Added UI for plant state

---
+++ Assets/StateManager.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StateManager : MonoBehaviour
+{
+    public Material healthy, unhealthy;
+    // Start is called before the first frame update
+    private state waterState, sunlightState, fertiliserAState, fertiliserBState;
+    public GameObject water, sun, fertA, fertB;
+
+    // Update is called once per frame
+    void Update()
+    {
+        waterState = GameObject.Find("Plant").GetComponent<GameController>().waterState;
+        sunlightState = GameObject.Find("Plant").GetComponent<GameController>().sunlightState;
+        fertiliserAState = GameObject.Find("Plant").GetComponent<GameController>().fertiliserAState;
+        fertiliserBState = GameObject.Find("Plant").GetComponent<GameController>().fertiliserBState;
+
+        if(sunlightState==state.HEALTHY) sun.GetComponent<Image>().material = healthy;
+        else sun.GetComponent<Image>().material = unhealthy;
+        if(waterState==state.HEALTHY) water.GetComponent<Image>().material = healthy;
+        else water.GetComponent<Image>().material = unhealthy;
+        if(fertiliserAState==state.HEALTHY) fertA.GetComponent<Image>().material = healthy;
+        else fertA.GetComponent<Image>().material = unhealthy;
+        if(fertiliserBState==state.HEALTHY) fertB.GetComponent<Image>().material = healthy;
+        else fertB.GetComponent<Image>().material = unhealthy;
+
+        water.transform.GetChild(0).gameObject.GetComponent<TextMeshProUGUI>().text = waterState.ToString();
+        sun.transform.GetChild(0).gameObject.GetComponent<TextMeshProUGUI>().text = sunlightState.ToString();
+        fertA.transform.GetChild(0).gameObject.GetComponent<TextMeshProUGUI>().text = fertiliserAState.ToString();
+        fertB.transform.GetChild(0).gameObject.GetComponent<TextMeshProUGUI>().text = fertiliserBState.ToString();
+    }
+}
